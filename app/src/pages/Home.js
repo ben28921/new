bb9e97e9f605e15f426193dfd87f4848d8dbbf08
ben23I <139108360+ben23I@users.noreply.github.com>
2023-09-12 16:35:46 +0800

    updata

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -4,15 +4,27 @@ import { Link, useNavigate, NavLink } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 import axios from "axios";
+import Moment from "moment";
 
-import { Button } from "@mui/material";
+import {
+	Button,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+} from "@mui/material";
 
 import { SERVER_ADDRESS } from "../utils/ServerParams";
 
 const Home = () => {
 	const token = localStorage.getItem("token");
 	const [tickets, setTickets] = useState([]);
-
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
+	const [selectedId, setSelectedId] = useState(null);
 	const navigate = useNavigate();
 
 	const navigateToAddTicket = () => {
@@ -31,6 +43,16 @@ const Home = () => {
 			.catch((err) => console.log(err));
 	};
 
+	function handleSelectTicket(id) {
+		// setSelectedId((selectedId) => (id === selectedId ? null : id));
+		// <Link to={`/posts/${id}`}></Link>;
+		navigate(`/posts/${id}`);
+		// alert(id);
+	}
+	// const handleSelectTicket = (id) => {
+	// 	setSelectedId((selectedId) => (id === selectedId ? null : id));
+	// };
+
 	const getAllticket = (a) => {
 		axios
 			.get(`${SERVER_ADDRESS}/api/v1/ticket`, {
@@ -40,6 +62,7 @@ const Home = () => {
 				},
 			})
 			.then((data) => {
+				setIsLoading(true);
 				setTickets(data.data.tickets);
 				// if (typeof data.data.tickets === "object") {
 				// 	setTickets([data.data.tickets]);
@@ -47,6 +70,7 @@ const Home = () => {
 				// } else {
 				// 	setTickets(data.data.tickets);
 				// }
+				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));
 	};
@@ -55,25 +79,58 @@ const Home = () => {
 		getAllticket();
 	}, []);
 
-	console.log("a", tickets);
+	console.log("Tickets", tickets);
+	// console.log("a", tickets);
 	return (
 		<div>
 			<Navbar />
 			<h1>Tickets</h1>
 			<Button onClick={navigateToAddTicket}>Add Ticket</Button>
-			console.log(tickets);
-			{/* {tickets.map((data) => data.r_id)} */}
-			{tickets.map((data, i) => (
-				<div key={i}>
-					TicketId:{data.r_id} ,title:
-					<Link to={`/posts/${data.r_id}`}>{data.r_title}</Link> , createAt :
-					{data.r_created_at}
-					<Button onClick={() => handleFinish(data.r_id)}>Finish</Button>
-					<hr />
-				</div>
-			))}
+
+			{isLoading ? (
+				<Loader />
+			) : (
+				<TicketTable
+					ticketsData={tickets}
+					onSelectTicket={handleSelectTicket}
+				/>
+			)}
 		</div>
 	);
 };
 
+function TicketTable({ ticketsData, onSelectTicket }) {
+	return (
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>TicketId</TableCell>
+						<TableCell>Title</TableCell>
+						<TableCell>Create At</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{ticketsData.map((data, i) => (
+						<TableRow onClick={() => onSelectTicket(data.r_id)} key={i}>
+							<TableCell>{data.r_id}</TableCell>
+							<TableCell>
+								<Link to={`/posts/${data.r_id}`}>{data.r_title}</Link>
+							</TableCell>
+							<TableCell>
+								{Moment(data.r_created_at).format("MM/DD/YYYY HH:mm")}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
+
+function TicketDetails({ selectedId }) {}
+
+function Loader() {
+	return <p>Loading...</p>;
+}
 export default Home;
