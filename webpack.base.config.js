75b864aae6da4f685f9e81b7e65026f70ba633eb
ben23I <139108360+ben23I@users.noreply.github.com>
2023-09-15 16:58:39 +0800

    update

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,85 +1,76 @@
-'use strict';
+"use strict";
 
-const Path = require('path');
+const Path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const ServerConfig = require(Path.join(__dirname, 'config.json'));
+const ServerConfig = require(Path.join(__dirname, "config.json"));
 
 module.exports = {
+	entry: {
+		// web app
+		app: Path.join(__dirname, "app", "src", "index.js"),
+	},
 
-    entry: {
-        // web app
-        app: Path.join(__dirname, 'app', 'src', 'index.js')
-    },
+	output: {
+		filename: "[name].js",
 
-    output: {
+		chunkFilename: "[name].bundle.js",
 
-        filename: '[name].js',
-        
-        chunkFilename: '[name].bundle.js',
+		path: Path.join(__dirname, "server", "dist"),
 
-        path: Path.join(__dirname, 'server', 'dist'),
+		publicPath: "/",
+	},
 
-        publicPath: '/'
-    },
+	plugins: [
+		// web frontend
+		new HtmlWebpackPlugin({
+			template: Path.join(__dirname, "app", "public", "index.html"),
+			favicon: Path.join(__dirname, "app", "public", "favicon.png"),
+		}),
+	],
 
-    plugins: [
-        // web frontend
-        new HtmlWebpackPlugin({
-            template: Path.join(__dirname, 'app', 'public', 'index.html'),
-            favicon: Path.join(__dirname, 'app', 'public', 'favicon.png'),
-        })
-    ],
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				include: [Path.join(__dirname, "app", "src")],
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							"@babel/preset-env",
+							"@babel/preset-react",
+							"@babel/preset-typescript",
+						],
+					},
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/, // styles files
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/, // to import images and fonts
+				loader: "url-loader",
+				options: {
+					limit: false,
+					name: "[name].[ext]",
+					outputPath: "assets/images/",
+				},
+			},
+		],
+	},
 
-    module: {
-        rules: [{
-            test: /\.(ts|js)x?$/,
-            include: [
-                Path.join(__dirname, 'app', 'src')
-            ],
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react',
-                        '@babel/preset-typescript'
-                    ]
-                }
-            }
-        },
-        {
-            test: /\.(sa|sc|c)ss$/, // styles files
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ],
-        },
-        {
-            test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/, // to import images and fonts
-            loader: "url-loader",
-            options: {
-                limit: false,
-                name: '[name].[ext]',
-                outputPath: 'assets/images/'
-            },
-        }]
-    },
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
 
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-
-    externals: {
-
-        'config': JSON.stringify({
-
-            application: {
-
-                apiEndpoint: ServerConfig.application.ajaxUrl
-            }
-        })
-    }
+	externals: {
+		config: JSON.stringify({
+			application: {
+				apiEndpoint: ServerConfig.application.ajaxUrl,
+			},
+		}),
+	},
 };
